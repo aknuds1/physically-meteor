5ae810156849bd7dab41d413eeb5dd2b209e957c
Arve Knudsen <arve.knudsen@gmail.com>
2015-10-12 14:44:03 +0200

    Implement more visuals

--- client/explore.js
+++ client/explore.js
@@ -46,6 +46,12 @@ Template.explore.helpers({
   practitioners: () => {
     return R.sortBy((p) => {return p.name}, Practitioners)
   },
+  specialtiesStr: function () {
+    return S.join(', ', this.areas)
+  },
+  institute: () => {
+    return 'NIMI'
+  },
   searchAxis: () => {
     let searchAxis = Session.get('searchQuery').axis
     if (searchAxis === 'all') {
@@ -80,9 +86,6 @@ Template.explore.helpers({
         icon: 'pulse2',
       },
     ])
-  },
-  fullName: function () {
-    return S.join(' ', this.names)
   },
   searchAxisPickerClass: () => {
     let selectedAxis = getSearchAxis()
